fedeb14f750dc795bb76ed7d04c43072d12acca8
Vincent Potucek <vpotucek@me.com>
2025-06-22 13:21:36 +0200

    GreetingService2

--- src/main/java/org/acme/GreetingService2.java
+++ src/main/java/org/acme/GreetingService2.java
@@ -9,6 +9,14 @@ public class GreetingService2 {
     @Inject
     private GreetingService1 greetingService1;
 
+    /**
+     * Exception in GreetingService1.java:17
+     * 	  15
+     * 	  16      public String hello() {
+     * 	→ 17          return greetingService2.hello();
+     * 	  18      }
+     * 	  19  }
+     */
     public String hello() {
 //        return greetingService1.hello();
         return "greetingService1.hello()";
